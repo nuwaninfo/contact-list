d7e196e8be34b756d05f244d5b06d6a85418fc11
Nuwan Fernando <nuwaninfo@gmail.com>
2024-02-21 22:00:19 +0200

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Table from './components/Table'
 function App() {
   const [contactList, setContactList] = useState([])
   const [contactName, setContactName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +22,34 @@ function App() {
       <h1>Contact List</h1>
       <form>
         <div>
-          Name: <input type='text' name='contactname' />
+          Name:
+          <input
+            type='text'
+            name='contactname'
+            value={contactName}
+            onChange={({ target }) => setContactName(target.value)}
+          />
+        </div>
+        <div>
+          Phone:
+          <input
+            type='text'
+            name='phone'
+            value={phone}
+            onChange={({ target }) => setPhone(target.value)}
+          />
+        </div>
+        <div>
+          email:
+          <input
+            type='text'
+            name='email'
+            value={phone}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </div>
+        <div>
+          <button type='submit'>Add</button>
         </div>
       </form>
       <Table contactList={contactList} />
